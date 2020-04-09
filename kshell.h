b44f4a5570e15218f4660ea49877dbdab13ea125
michedomingo <michedomingo@gmail.com>
2020-04-08 18:51:38 -0700

    Add header

---
+++ kshell.h
@@ -0,0 +1,24 @@
+#ifndef KSHELL_H
+#define KSHELL_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/*str_helpers.c */
+unsigned int num_words(char *line);
+int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
+
+/* error.c */
+
+/*kshell.c */
+void check_prompt();
+
+/*free_mem.c */
+void free_av(char **cmds);
+
+#endif /*KSHELL_H */

068eac4b21f5db87f9925898cb631884fa0727d3
djso89 <danielso89@gmail.com>
2020-04-08 19:04:35 -0700

    added new prototype: _strcmp

--- kshell.h
+++ kshell.h
@@ -1,5 +1,6 @@
 #ifndef KSHELL_H
 #define KSHELL_H
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -12,13 +13,16 @@
 unsigned int num_words(char *line);
 int _strlen(char *s);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 
 /* error.c */
+
 
 /*kshell.c */
 void check_prompt();
 
 /*free_mem.c */
 void free_av(char **cmds);
+
 
 #endif /*KSHELL_H */

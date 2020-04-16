3bbfc95782a0c4332eb44fe5cabd96cd979e754f
djso89 <danielso89@gmail.com>
2020-04-15 19:16:38 -0700

    reformated

--- kshell.h
+++ kshell.h
@@ -12,30 +12,27 @@
 
 void free_upto_n(char **av, unsigned int n);
 void free_av(char **cmds);
-
+/*kshell.c*/
 void check_prompt(void);
-
 char *get_input();
+char *check_input(char *cmd, char **env);
 
+/*avtok.c*/
 unsigned int num_words(char *line, char delim);
-
 char **line_to_av(char *line);
 
+/*str_helpers.c*/
 int _strncmp(const char *s1, const char *s2, size_t n);
-
 unsigned int _strlen(char *s);
-
 char *_strncpy(char *dest, char *src, int n);
-
 char *_strdup(char *str);
-
 char *_getenv(const char *name, char **env);
 
+/*this is for handling ctrl - c*/
 void sigintHandler(int sig_num);
 
 
-/*directory helpers*/
-
+/*dir_helpers.c*/
 unsigned int get_num_dir(char *dir);
 char *get_dir_cmd(char *dir, char *cmd);
 char **get_dir(char **env);

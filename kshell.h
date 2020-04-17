bf80c41eabdf86c56002ca2646e92359d7e4e307
michedomingo <michedomingo@gmail.com>
2020-04-16 22:39:14 -0700

    Update header

--- kshell.h
+++ kshell.h
@@ -10,26 +10,20 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-
-/*free_mem.c*/
-void free_upto_n(char **av, unsigned int n);
-void free_av(char **cmds);
-void free_all(char *line, char **av);
-void free3(char *line, char **av, char *fn);
 /*kshell.c*/
 void check_prompt(void);
 char *get_input();
 char *check_input(char *cmd, char **env);
 void check_exit(int ext_stat, char *cmd, char *line, char **av);
+
 /*avtok.c*/
 unsigned int num_words(char *line, char delim);
 char **line_to_av(char *line);
 
-/*str_helpers.c*/
-int _strncmp(const char *s1, const char *s2, size_t n);
-unsigned int _strlen(char *s);
-char *_strncpy(char *dest, char *src, int n);
-char *_strdup(char *str);
+/*dir_helpers.c*/
+unsigned int get_num_dir(char *dir);
+char *get_dir_cmd(char *dir, char *cmd);
+char **get_dir(char **env);
 
 /*env_helpers.c*/
 char *_getenv(const char *name, char **env);
@@ -44,16 +38,22 @@ void show_error(char *argv, char *cmd, int e_num, int cmd_num);
 void errno_file(int *e_num, char *argv0, char **av, char *line, int cmd_num);
 void errno_perm(int *es, char *argv0, char **av, char *line, int cn, char *fn);
 
-/*dir_helpers.c*/
-unsigned int get_num_dir(char *dir);
-char *get_dir_cmd(char *dir, char *cmd);
-char **get_dir(char **env);
-
 /*exit_helpers.c*/
 int get_exit_stat(int stat);
 
 /*execute.c*/
 int execute(pid_t pgm, char **av, char *line, char **env, char *fname);
 
+/*free_mem.c*/
+void free_upto_n(char **av, unsigned int n);
+void free_av(char **cmds);
+void free_all(char *line, char **av);
+void free3(char *line, char **av, char *fn);
+
+/*str_helpers.c*/
+int _strncmp(const char *s1, const char *s2, size_t n);
+unsigned int _strlen(char *s);
+char *_strdup(char *str);
+char *_strncpy(char *dest, char *src, int n);
 
 #endif /*KSHELL_H */
